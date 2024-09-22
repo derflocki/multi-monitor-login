d03d81f40bd01856462cb4a1a0e62e3698ca77fb
Florian Dorn <florian.dorn@boku.ac.at>
2024-09-22 10:24:47 +0200

    fist version working with lock-screen

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 ### Help on running
 https://gjs.guide/extensions/development/creating.html
 
+https://gitlab.gnome.org/GNOME/gnome-shell/-/blob/main/js/ui/screenShield.js?ref_type=heads
+
 ```bash
-env MUTTER_DEBUG_DUMMY_MODE_SPECS=1440x768 dbus-run-session -- gnome-shell --nested --wayland
+env MUTTER_DEBUG_NUM_DUMMY_MONITORS=2 MUTTER_DEBUG_DUMMY_MODE_SPECS=1440x768 dbus-run-session -- gnome-shell --nested --wayland
 ```
 session-modes
--- multiMonitorLogin.js
+++ multiMonitorLogin.js
@@ -1,41 +1,67 @@
-import Atspi from 'gi://Atspi';
 import * as Main from 'resource:///org/gnome/shell/ui/main.js';
+//import {Monitor} from 'resource:///org/gnome/shell/ui/layout.js';
 import {getPointerWatcher} from 'resource:///org/gnome/shell/ui/pointerWatcher.js';
 
 export {MultiMonitorLogin};
 
 const MultiMonitorLogin = class {
+    /**
+     * The complete Triforce, or one or more components of the Triforce.
+     * @typedef {Object} Monitor
+     * @property {int} index - Indicates whether the Wisdom component is present.
+     * @property {int} x - Indicates whether the Courage component is present.
+     * @property {int} y - Indicates whether the Power component is present.
+     * @property {int} width - Indicates whether the Wisdom component is present.
+     * @property {int} height - Indicates whether the Wisdom component is present.
+     * @property {int} geometry_scale - Indicates whether the Wisdom component is present.
+     */
+    /** @type {Monitor}*/
+    lastMonitor = {index: -1, x: 0, y: 0, width: 0, height: 0, geometry_scale: 0};
 
-    lastMonitor = {x: 0, y: 0, width: 0, height: 0};
     pointerWatcherRef;
 
+    /**
+     *
+     * @param {int} mouse_x
+     * @param {int} mouse_y
+     * @private
+     */
     _trackMouse(mouse_x, mouse_y) {
+        /** @type {Monitor}*/
         let current = this.getMonitorAtPosition(mouse_x, mouse_y);
-        if(!current) {
+        if (!current) {
             return;
         }
-        /*
         //are we on another monitor?
         if ((current.x === this.lastMonitor.x) &&
-            (current.y === this.lastMonitor.x) &&
+            (current.y === this.lastMonitor.y) &&
             (current.width === this.lastMonitor.width) &&
-            (current.height === this.lastMonitor.height)
+            (current.height === this.lastMonitor.height) &&
+            (current.index === this.lastMonitor.index)
         ) {
             return;
         }
-        */
+        console.log("New Monitor!!!");
+        console.log(JSON.stringify(current));
+        console.log(JSON.stringify(this.lastMonitor));
 
         //move the relevant actor ot the current monitor
-        this.moveActors(current, this.lastMonitor);
+        this.updateActors(current, this.lastMonitor);
         this.lastMonitor = current;
     };
 
+    /**
+     *
+     * @param {int} mouse_x
+     * @param {int} mouse_y
+     * @returns {undefined|Monitor}
+     */
     getMonitorAtPosition(mouse_x, mouse_y) {
         let monitor = Main.layoutManager.monitors.filter(m => {
             return (m.x < mouse_x) && (mouse_x < m.x + m.width) &&
                 (m.y < mouse_y) && (mouse_y < m.y + m.height);
         });
-        if(monitor.length == 1) {
+        if (monitor.length == 1) {
             return monitor[0];
         }
         return undefined;
@@ -45,8 +71,8 @@ const MultiMonitorLogin = class {
         this.lastMonitor = {x: 0, y: 0, width: 0, height: 0};
         console.log("multi-monitor-login@derflocki.github.com enable");
         let pointerWatcher = getPointerWatcher();
-        this.pointerWatcherRef = pointerWatcher.addWatch(500, (x, y) => {
-            this._trackMouse(x,y);
+        this.pointerWatcherRef = pointerWatcher.addWatch(100, (x, y) => {
+            this._trackMouse(x, y);
         });
         console.log("multi-monitor-login@derflocki.github.com enable complete");
     }
@@ -56,19 +82,51 @@ const MultiMonitorLogin = class {
      */
     disable() {
         console.log("multi-monitor-login@derflocki.github.com disable");
-        if(this.pointerWatcherRef) {
+        if (this.pointerWatcherRef) {
             this.pointerWatcherRef.remove();
         }
         console.log("multi-monitor-login@derflocki.github.com disable complete");
     }
 
-    moveActors(current, last) {
-        //Main.notify('Mouse moved to another monitor', 'New Monitor ' + JSON.stringify(current));
+    /**
+     * Move the relevant Actors on the current screen
+     * @param {Monitor} current - The title of the book.
+     * @param {Monitor} last - The author of the book.
+     */
+    updateActors(current, last) {
         console.log("multi-monitor-login@derflocki.github.com: New Monitor " + JSON.stringify(current));
-        if(!Main.screenShield) {
-            return;
+
+        //The Lock screen
+        //Main.screenShield: https://github.com/GNOME/gnome-shell/blob/main/js/ui/screenShield.js
+        if ((Main.screenShield && Main.screenShield._dialog)) {
+            //Main.screenShield._dialog: https://github.com/GNOME/gnome-shell/blob/main/js/ui/unlockDialog.js
+            this.moveActor(Main.screenShield._dialog, current)
         }
-        console.log("multi-monitor-login@derflocki.github.com: " + Main.screenShield);
-        console.log("multi-monitor-login@derflocki.github.com: " + Main.screenShield._dialog.x);
+        //styleClass: "unlock-dialog"
+        //styleClass: "login-dialog"
+        //global.stage
+    }
+
+    /**
+     *
+     * @param {}Clutter.Actor} actor
+     * @param {array} styleClasses
+     */
+    findStyleClassRecursive(actor, styleClasses) {
+        //if(actor.styleClass)
+    }
+    moveActor(_dialog, monitor) {
+        console.log("multi-monitor-login@derflocki.github.com: _dialog: " + Main.screenShield._dialog);
+        _dialog.get_children().forEach((child) => {
+            console.log("multi-monitor-login@derflocki.github.com: _dialog.child: " + child);
+            child.get_constraints().forEach((constraint) => {
+                //constraint: https://github.com/GNOME/gnome-shell/blob/main/js/ui/layout.js#L39
+                /** @type {ClutterConstraint}*/
+                //MonitorConstraint
+                //TODO: check for the "object instance wrapper GType:Gjs_ui_layout_MonitorConstraint jsobj@0x366081898588 native@0x5626ed2f6930"
+                console.log("multi-monitor-login@derflocki.github.com: ClutterConstraint: " + constraint);
+                constraint.index = monitor.index;
+            });
+        });
     }
 };
